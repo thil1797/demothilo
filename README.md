3f8d82bf3e862a288e561644c01a17d7d8118a4e
thil1797 <thil1797@stud.kea.dk>
2022-03-28 12:45:50 +0200

    Final changes
    Final Changes

--- README.md
+++ README.md
@@ -1,8 +1,2 @@
-# demothilo
-Demo of GitHub
-
-## This is something
-- Point 1
-- Point 2
-
-VfB 1893 
+# Delivery1
+From Thilo Merk KEA-ITBE2020
---
+++ app.py
@@ -0,0 +1,139 @@
+# ***************************************
+# Imports
+# ***************************************
+# Dash
+import dash
+from dash import html
+from dash import dcc
+from dash.dependencies import Input, Output
+
+# Div.
+import pandas as pd
+import numpy as np
+import calendar
+
+# Plotly
+import plotly.express as px
+import plotly.graph_objects as go
+
+# ***************************************
+# Get data
+# ***************************************
+import datamodel
+order = datamodel.get_data()
+df_year = datamodel.get_year()
+df_month = datamodel.get_month()
+
+# ***************************************
+# Diagram - Employee Sales
+# ***************************************
+fig_employee = px.bar(order, 
+    x='emp_name', y='total', 
+    color='type', text='total', title='Sales by Employee',
+    hover_data=[],
+    labels={'total':'Total sales', 'emp_name':'Employee', 'type':'Product Type'})
+fig_employee.update_traces(texttemplate='%{text:.2s}', textposition='outside')
+fig_employee.update_layout(uniformtext_minsize=8, uniformtext_mode='hide', xaxis_tickangle=45)
+
+# ***************************************
+# Diagram - Product Sales
+# ***************************************
+fig_product = px.bar(order, 
+    x='productname', y='total', 
+    color='type', text='total', title='Sales by Product',
+    hover_data=[],
+    labels={'total':'Total sales', 'productname':'Product', 'type':'Product Type'})
+fig_product.update_traces(texttemplate='%{text:.2s}', textposition='outside')
+fig_product.update_layout(uniformtext_minsize=8, uniformtext_mode='hide', xaxis_tickangle=45)
+
+# ***************************************
+# Activate the app
+# ***************************************
+#app = dash.Dash(__name__)
+
+dash_app = dash.Dash(__name__)
+app = dash_app.server
+
+# ***************************************
+# Layout
+# ***************************************
+dash_app.layout = html.Div(
+    children=[html.H2('KEA BE-IT2020 Dashboard - Thilo Merk', style={
+            'text-align': 'center'
+        },),
+        html.Div(className='row', style={
+            'text-align': 'center'
+        },
+                children=[
+                    html.Div(className='div-user-controls',
+                            children=[
+                                html.P('Select filters from dropdown'),
+
+                    html.Div(children="Month", className="menu-title"),
+                            dcc.Dropdown(
+                                id='drop_month',
+                                options=[{'label':selectmonth, 'value':selectmonth} for selectmonth in df_month['monthnames']],
+                            ),
+                    html.Div(children="Year", className="menu-title"),
+                            dcc.Dropdown(
+                                id='drop_year',
+                                options=[{'label':selectyear, 'value':selectyear} for selectyear in df_year]
+                            ),
+                            ]
+                    ),
+                    html.Div(className='div-for-charts bg-grey',
+                            children=[html.H2('Sales by Employees'
+        ),
+                                dcc.Graph(id="sales_employee", figure=fig_employee)
+                            ]
+                    ),
+                     html.Div(className='div-for-charts bg-grey',
+                            children=[html.H2('Sales by Product'
+        ),
+                                dcc.Graph(id="sales_products", figure=fig_product)
+                            ]
+                    )
+                ]
+            )
+        ]
+)
+
+# ***************************************
+# Callbacks
+# ***************************************
+# Output er diagrammet
+# Input er DropDown
+@dash_app.callback(Output('sales_employee', 'figure'),
+              [Input('drop_month', 'value')],
+              [Input('drop_year', 'value')])
+
+def update_graph(drop_month, drop_year):
+    if drop_year:
+        if drop_month:
+            # Data i både drop_month og drop_year
+            order_fig1 = order.loc[(order['orderyear'] == drop_year) & (order['ordermonth'] == drop_month)]
+        else:
+            # Data i drop_year. men ikke drop_month
+            order_fig1 = order.loc[order['orderyear'] == drop_year]
+    else:
+        if drop_month:
+            # Data i drop_month, men ikke drop_year
+            order_fig1 = order.loc[order['ordermonth'] == drop_month]
+        else:
+            # Ingen data - ikke noget valgt
+            order_fig1 = order
+        
+    return {'data':[go.Bar(
+        x = order_fig1['emp_name'],
+        y = order_fig1['total']
+            )
+        ]
+    }
+
+
+
+# ***************************************
+# Run the app
+# ***************************************
+if __name__ == '__main__':
+    dash_app.run_server(debug=True)
